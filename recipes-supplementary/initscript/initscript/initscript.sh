edb42d10aaf0f8ebf0d90777104f88b5360db1ca
SrijanNandi <srijan.nandi@gmail.com>
2020-08-05 05:05:50 +0000

    Changed login username of graylog

--- recipes-supplementary/initscript/initscript/initscript.sh
+++ recipes-supplementary/initscript/initscript/initscript.sh
@@ -70,7 +70,7 @@ rm -rf /tmp/newfile.temp*
 
 GRAYLOG_CONF="/etc/graylog/server/server.conf"
 SEC_PWD=`/usr/bin/ranpwd --alphanum --lower 96`
-ROOT_PWD=`/bin/echo -n "grays" | /usr/bin/tr -d '\n' | /usr/bin/sha256sum | /usr/bin/cut -d" " -f1`
+ROOT_PWD=`/bin/echo -n "rays" | /usr/bin/tr -d '\n' | /usr/bin/sha256sum | /usr/bin/cut -d" " -f1`
 
 if [ -f ${GRAYLOG_CONF} ]; then
 	cp -r ${GRAYLOG_CONF} ${GRAYLOG_CONF}.ORIG
